a59ceb87a4d7a1e38cba5cd04274931fd52b11da
onbit-syn <onbitsyn@gmail.com>
2021-05-24 09:28:08 +0530

    debugging to increase performance

--- playground/node-streaming-server/api/controllers/video-stream.js
+++ playground/node-streaming-server/api/controllers/video-stream.js
@@ -6,7 +6,8 @@ const fs = require('fs'),
     path = require('path'),
 
     videoDir = path.join(__dirname, '../../static/videos'),
-    MAX_CHUNK_SIZE = 500000; // 500 KB
+    MAX_CHUNK_SIZE = 1000000, // 1 MB,
+    VIDEO_SIZE = 63614462;
 
 module.exports = {
     schema: {
@@ -30,45 +31,29 @@ module.exports = {
         const requestRange = req.headers.range,
             { videoName } = req.params,
             videoPath = path.join(videoDir, `/${videoName}`);
-
-        // eslint-disable-next-line security/detect-non-literal-fs-filename
-        fs.promises.stat(videoPath)
-            .then(function (stat) {
-                return stat.size;
-            })
-            .then(function (videoSize) {
+        
                 // eslint-disable-next-line no-unused-vars
-                const [type, range] = requestRange.split('=');
+            const [type, range] = requestRange.split('=');
 
-                let [start, end] = range.split('-')
-                    .map((value) => parseInt(value, 10));
+            let [start, end] = range.split('-')
+                .map((value) => parseInt(value, 10));
 
-                if (!end) {
-                    end = Math.min(start + MAX_CHUNK_SIZE, videoSize - 1);
-                } else {
-                    end = MATH.min(Math.min(end, start 
-                        + MAX_CHUNK_SIZE),  videoSize - 1);
-                }
+            if (!end) {
+                end = Math.min(start + MAX_CHUNK_SIZE, VIDEO_SIZE - 1);
+            } else {
+                end = MATH.min(Math.min(end, start 
+                    + MAX_CHUNK_SIZE),  VIDEO_SIZE - 1);
+            }
 
-                reply.raw.writeHead(206, {
-                    'Content-Range': `bytes ${start}-${end}/${videoSize}`,
-                    'Accept-Ranges': 'bytes',
-                    'Content-Length': `${end - start + 1}`,
-                    'Content-Type': 'video/mp4'
-                });
-                
-                const videoStream = fs.createReadStream(videoPath, { start, end });
-
-                videoStream.pipe(reply.raw);
-
-            })
-            .catch(function (err) {
-                req.log.error(err);
-                reply
-                    .code(500)
-                    .send({ msg: 'Some internal server error.' +
-                     'Please open issue at ' +
-                     'https://github.com/onbit-syn/random-learning/issues' });
+            reply.raw.writeHead(206, {
+                'Content-Range': `bytes ${start}-${end}/${VIDEO_SIZE}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': `${end - start + 1}`,
+                'Content-Type': 'video/mp4'
             });
+                
+            const videoStream = fs.createReadStream(videoPath, { start, end });
+
+            videoStream.pipe(reply.raw);
     }
 };
--- playground/node-streaming-server/bin/deploy-local.sh
+++ playground/node-streaming-server/bin/deploy-local.sh
@@ -3,10 +3,10 @@
 # error out if some error occured
 set -e
 # build docker image
-docker build -t "onbit-syn/node-streaming-server" .
+docker build -t "onbitsyn/node-video-streaming-server" .
 
 # run docker container
-docker run -d -p 4324:3658 onbit-syn/node-streaming-server
+docker run -d -p 4324:3658 onbitsyn/node-streaming-server
 
 # on success
 echo "Application deployed successfully, please open http://localhost:4324/ on browser"
