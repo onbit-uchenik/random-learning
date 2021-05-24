3cae09a42865d93248dc7190b2dc8646ddfdcdb8
onbit-syn <onbitsyn@gmail.com>
2021-05-24 09:32:31 +0530

    removed typo error

--- playground/node-streaming-server/bin/deploy-local.sh
+++ playground/node-streaming-server/bin/deploy-local.sh
@@ -6,7 +6,7 @@ set -e
 docker build -t "onbitsyn/node-video-streaming-server" .
 
 # run docker container
-docker run -d -p 4324:3658 onbitsyn/node-streaming-server
+docker run -d -p 4324:3658 onbitsyn/node-video-streaming-server
 
 # on success
 echo "Application deployed successfully, please open http://localhost:4324/ on browser"
